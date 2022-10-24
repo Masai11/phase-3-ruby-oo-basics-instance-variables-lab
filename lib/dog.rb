ec9b9ccb9b0533b4bd83a9266e0443346259791e
Masai11 <roy.kiplagat@student.moringaschool.com>
2022-10-24 16:17:51 +0300

    lab complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,32 @@
 class Dog
-
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+    def name
+        @this_dogs_name
+    end
+    
 end
+lassie = Dog.new
+lassie.name = "Lassie"
+puts lassie.name
+
+# class Dog
+#     # setter method
+#     def name=(value)
+
+#         #assign the @name instance variable of whatever is being passed as an argument
+#         @name = value
+
+#     end
+#     #getter method
+#     def name
+#         #accessing the data from @name instance variable and return it
+#         @name
+
+#     end
+# end
+# odie = Dog.new
+# odie.name = "Odie"
+
+# puts odie.name
